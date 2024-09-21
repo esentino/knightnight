7b1252906c46f5fa1fd0cd06dd65afe13b7463d4
janusz.janowski <januszjanowski@outlook.com>
2024-09-21 22:31:59 +0200

    Initial Commit

---
+++ knightnight/knightnightApp.swift
@@ -0,0 +1,17 @@
+//
+//  knightnightApp.swift
+//  knightnight
+//
+//  Created by Janusz Janowski on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct knightnightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
